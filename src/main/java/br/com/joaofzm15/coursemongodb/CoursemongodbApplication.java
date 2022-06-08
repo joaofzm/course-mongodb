2292e8dd3e4f140c0f907ab32d21a1981cfd60db
joao <joao.fzm@hotmail.com>
2022-06-08 16:00:16 -0300

    Project created

---
+++ src/main/java/br/com/joaofzm15/coursemongodb/CoursemongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.coursemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemongodbApplication.class, args);
+	}
+
+}
