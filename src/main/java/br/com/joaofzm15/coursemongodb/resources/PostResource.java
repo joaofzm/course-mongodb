f24da6574c3facbf549276c3ff0de06c5df1832a
joao <joao.fzm@hotmail.com>
2022-06-12 14:07:04 -0300

    Add findById functionality to post

---
+++ src/main/java/br/com/joaofzm15/coursemongodb/resources/PostResource.java
@@ -0,0 +1,26 @@
+package br.com.joaofzm15.coursemongodb.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaofzm15.coursemongodb.domain.Post;
+import br.com.joaofzm15.coursemongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	PostService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+
+}
---
+++ src/main/java/br/com/joaofzm15/coursemongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package br.com.joaofzm15.coursemongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.joaofzm15.coursemongodb.domain.Post;
+import br.com.joaofzm15.coursemongodb.repositories.PostRepository;
+import br.com.joaofzm15.coursemongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+
+
+}
