8a535c63a1b414e86043228cd9fd3cc337da3582
joao <joao.fzm@hotmail.com>
2022-06-13 15:17:04 -0300

    Add a second search by post title, with @Query anotation

--- src/main/java/br/com/joaofzm15/coursemongodb/repositories/PostRepository.java
+++ src/main/java/br/com/joaofzm15/coursemongodb/repositories/PostRepository.java
@@ -3,11 +3,17 @@ package br.com.joaofzm15.coursemongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import br.com.joaofzm15.coursemongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//?0 means first parameter
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitleContainingIgnoreCaseQuery(String text);
+	
 
 }
--- src/main/java/br/com/joaofzm15/coursemongodb/services/PostService.java
+++ src/main/java/br/com/joaofzm15/coursemongodb/services/PostService.java
@@ -24,6 +24,6 @@ public class PostService {
 
 
 	public List<Post> findByTitleContainingIgnoreCase(String text) {
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.findByTitleContainingIgnoreCaseQuery(text);
 	}
 }
